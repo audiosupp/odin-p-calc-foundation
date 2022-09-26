8b68f1322902cc32b5486893e04b1bd30b409539
audiosupp <shkroling@gmail.com>
2022-09-27 00:58:23 +0300

    fix: html. add: some js functions

---
+++ main.js
@@ -0,0 +1,61 @@
+let currentNum = "";
+let previousNum = "";
+let operator = "";
+
+const currentDisplayNumber = document.querySelector('.currentNumber');
+const previousDisplayNumber = document.querySelector('.previousNumber');
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", calculate);
+
+const decimal = document.querySelector(".decimal");
+
+const clear = document.querySelector(".clear");
+
+const numberButtons = document.querySelectorAll('.number');
+
+const operators = document.querySelectorAll('.operator');
+
+numberButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        handleNumber(e.target.textContent);
+    });
+});
+
+function handleNumber(number) {
+    console.log(number);
+    if (currentNum.length <= 10) {
+        currentNum += number;
+        currentDisplayNumber.textContent = currentNum;
+    }
+}
+
+operators.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        handleOperator(e.target.textContent);
+    });
+});
+
+function handleOperator(op) {
+    operator = op;
+    previousNum = currentNum;
+    previousDisplayNumber.textContent = previousNum + " " + operator;
+    currentNum = "";
+    currentDisplayNumber.textContent = "";
+}
+
+function calculate() {
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
+
+    if (operator === "+") {
+        previousNum = previousNum + currentNum;
+    } else if (operator === "-") {
+        previousNum = previousNum - currentNum;
+    } else if (operator === "*") {
+        previousNum = previousNum * currentNum;
+    }
+
+    previousDisplayNumber.textContent = "";
+    currentDisplayNumber.textContent = previousNum;
+}
